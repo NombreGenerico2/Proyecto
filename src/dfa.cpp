fd4dfa4398a46e08fa6a22b1a10f23359e4f095b
Miunmn <esteban.principe@utec.edu.pe>
2020-11-08 00:31:49 -0500

    dfacpp

--- src/dfa.cpp
+++ src/dfa.cpp
@@ -9,6 +9,7 @@ std::istream& operator>>(std::istream& is, dfa& _dfa)
 	_dfa.states = std::vector<dfa::state>(n);
 	_dfa.initial = initial;
 
+	//Estados de aceptacion
 	for(int i = 0; i < finals_n; ++i)
 	{
 		int accepting;
@@ -17,6 +18,7 @@ std::istream& operator>>(std::istream& is, dfa& _dfa)
 		_dfa.states[accepting].accepting = true;
 	}
 
+	//Transiciones
 	for(int i = 0; i < n*2; ++i)
 	{
 		int p, a, q;
@@ -99,4 +101,81 @@ dfa dfa::brzozowski()
 	//std::cerr << n;
 	//return n.powerset();
 	return reverse_edges().powerset().reverse_edges().powerset();
+}
+void dfa::stateEquivalence()
+{
+	using namespace std;
+	using ll = long long;
+	using vi = std::vector<int>;
+	using ii = pair< int,int >;
+
+	int n = states.size();
+
+
+
+	bool distinguidos[n][n];
+
+
+	for (int i = 0 ; i < n ; i++){
+		for (int j = 0 ; j < n; j++){
+			distinguidos[i][j] = 0;	
+		}
+	}
+	
+		for (int i = 0 ; i < n ; i++){
+		for (int j = 0 ; j < n; j++){
+			cout<< distinguidos[i][j]<<" ";	
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+
+	for(int i = 0; i < n; i++){
+        if(states[i].accepting){
+            for(int j = i + 1; j < n; j++){
+                if(!states[j].accepting){
+                    //lleno la fila que le corresponde 
+                    //a este estado de aceptacion
+                    distinguidos[i][j] = true;
+                }
+            }
+            for(int k = i + 1; k < n; k++){
+                if(!states[k].accepting){
+                    //lleno la columna que le corresponde
+                    //a este estado de aceptacion
+                    distinguidos[k][i] = true;
+                }
+            }
+        }
+    }
+		for (int i = 0 ; i < n ; i++){
+		for (int j = 0 ; j < n; j++){
+			cout<< distinguidos[i][j]<<" ";	
+		}
+		cout<<endl;
+	}
+	//Estados finales
+	/*
+    for(int i = 0; i < n; i++){
+    if(states[i].accepting){
+        for(int j = i + 1; j < n; j++){
+            if(!accept[j]){
+                //lleno la fila que le corresponde 
+                //a este estado de aceptacion
+                distinguidos[i][j] = true;
+            }
+        }
+        for(int k = i; k < n; k++){
+            if(!accept[k]){
+                //lleno la columna que le corresponde
+                //a este estado de aceptacion
+                distinguidos[k][i] = true;
+            	}
+        	}
+    	}
+	}*/
+	
+	//int modificaciones = 0;
+
+	
 }
